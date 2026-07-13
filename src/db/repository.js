dee835e44e14b4b1763c2d6a48a8aceb38753a1a
tsukiyukihira01-hue <tsukiyukihira01@gmail.com>
2026-07-13 15:07:13 +0700

    Refactor Repository class for improved readability

--- src/db/repository.js
+++ src/db/repository.js
@@ -1,59 +1,53 @@
 import { COMPONENT_REGISTRY } from '../core/component.js'
 import { migrateRecord, stampRecord, SCHEMA_VERSION } from './migrations.js'
-
-export class Repository {
-  constructor(dbName='ecs_world'){
-    this.dbName=dbName; this.db=null
-  }
+export class Repository{
+  constructor(dbName='ecs_world'){ this.dbName=dbName; this.db=null }
   async init(){
-    return new Promise((res, rej)=>{
-      const req = indexedDB.open(this.dbName, SCHEMA_VERSION)
-      req.onupgradeneeded = (e)=>{
-        const db = e.target.result
-        for(const [name, Ctor] of COMPONENT_REGISTRY){
-          const sname = Ctor.tableName()
-          if(!db.objectStoreNames.contains(sname)) db.createObjectStore(sname, {keyPath:'entityId'})
+    return new Promise((res,rej)=>{
+      const req=indexedDB.open(this.dbName, SCHEMA_VERSION)
+      req.onupgradeneeded=(e)=>{
+        const db=e.target.result
+        for(const [,Ctor] of COMPONENT_REGISTRY){
+          const sname=Ctor.tableName()
+          if(!db.objectStoreNames.contains(sname)) db.createObjectStore(sname,{keyPath:'entityId'})
         }
       }
-      req.onsuccess = (e)=>{ this.db=e.target.result; res() }
-      req.onerror = rej
+      req.onsuccess=(e)=>{ this.db=e.target.result; res() }
+      req.onerror=()=>rej(req.error)
     })
   }
   async bulkSave(world){
     if(!this.db) await this.init()
-    const tx = this.db.transaction([...COMPONENT_REGISTRY.values()].map(c=>c.tableName()), 'readwrite')
-    for(const [name, Ctor] of COMPONENT_REGISTRY){
-      const storeName = Ctor.tableName()
+    const tx=this.db.transaction([...COMPONENT_REGISTRY.values()].map(c=>c.tableName()),'readwrite')
+    for(const [name,Ctor] of COMPONENT_REGISTRY){
+      const storeName=Ctor.tableName()
       if(!world.store.has(name)) continue
-      const map = world.store.get(name)
-      const os = tx.objectStore(storeName)
+      const map=world.store.get(name)
+      const os=tx.objectStore(storeName)
       os.clear()
-      for(const [eid, comp] of map){
-        const stamped = stampRecord(comp)
-        os.put({entityId:eid, ...stamped})
-      }
+      for(const [eid,comp] of map){ os.put({entityId:eid, ...stampRecord(comp)}) }
     }
     return new Promise((res,rej)=>{ tx.oncomplete=res; tx.onerror=()=>rej(tx.error) })
   }
   async loadAll(world){
     if(!this.db) await this.init()
-    const names = [...COMPONENT_REGISTRY.values()].map(c=>c.tableName())
-    const tx = this.db.transaction(names, 'readonly')
-    for(const [name, Ctor] of COMPONENT_REGISTRY){
-      const storeName = Ctor.tableName()
+    const names=[...COMPONENT_REGISTRY.values()].map(c=>c.tableName())
+    const tx=this.db.transaction(names,'readonly')
+    for(const [name,Ctor] of COMPONENT_REGISTRY){
+      const storeName=Ctor.tableName()
       if(!tx.objectStoreNames.contains(storeName)) continue
-      const os = tx.objectStore(storeName)
-      const req = os.getAll()
-      await new Promise((res, rej)=>{
+      const os=tx.objectStore(storeName)
+      const req=os.getAll()
+      await new Promise((res,rej)=>{
         req.onsuccess=()=>{
           for(let row of req.result){
-            row = migrateRecord(storeName, row)
-            const {entityId, _v, ...data}=row
+            row=migrateRecord(storeName,row)
+            const {entityId,_v,...data}=row
             try{
-              const comp = Ctor.fromJSON(data)
+              const comp=Ctor.fromJSON(data)
               world.addComponent(entityId, comp)
               if(entityId>=world.nextId) world.nextId=entityId+1
-            }catch(e){ console.warn('migrate skip', storeName, entityId, e) }
+            }catch(e){ console.warn('migrate skip',e) }
           }
           res()
         }
