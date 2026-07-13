6bc5cad781e43a54267d2f354eedb34a8415c9cf
tsukiyukihira01-hue <tsukiyukihira01@gmail.com>
2026-07-13 17:44:14 +0700

    Refactor WritePolicy and CANONICAL_OWNERS export

--- src/core/policy.js
+++ src/core/policy.js
@@ -1,25 +1,2 @@
-export class WritePolicy{
-  constructor(){ this.globalOwners=new Map() }
-  check(sys){
-    for(const C of sys.writes||[]){
-      const n=C.name
-      if(this.globalOwners.has(n)){
-        const o=this.globalOwners.get(n)
-        if(o!==sys.constructor.name) return `GLOBAL CONFLICT: ${sys.constructor.name} wants ${n} but ${o} already owns it. Use events.`
-      }
-    }
-    return null
-  }
-  claim(sys){ for(const C of sys.writes||[]) this.globalOwners.set(C.name, sys.constructor.name) }
-  dump(){ return [...this.globalOwners.entries()].map(([c,o])=>`${c}->${o}`).join(', ') }
-}
-export const CANONICAL_OWNERS={
-  Position:'MovementSystem',
-  Army:'CombatSystem',
-  Province:'EconomySystem',
-  Economy:'EconomySystem',
-  Relation:'DiplomacySystem',
-  TradeRoute:'TradeSystem',
-  Building:'BuilderSystem',
-  MoveOrder:'MovementSystem'
-}
+export class WritePolicy{constructor(){this.globalOwners=new Map()}check(sys){for(const C of sys.writes||[]){const n=C.name;if(this.globalOwners.has(n)){const o=this.globalOwners.get(n);if(o!==sys.constructor.name) return `GLOBAL CONFLICT: ${sys.constructor.name} wants ${n} but ${o} already owns it. Use events.`}}return null}claim(sys){for(const C of sys.writes||[]) this.globalOwners.set(C.name,sys.constructor.name)}dump(){return [...this.globalOwners.entries()].map(([c,o])=>`${c}->${o}`).join(', ')}}
+export const CANONICAL_OWNERS={Position:'MovementSystem',Army:'CombatSystem',Province:'EconomySystem',Economy:'EconomySystem',Relation:'DiplomacySystem',TradeRoute:'TradeSystem',Building:'BuilderSystem',MoveOrder:'MovementSystem'}
